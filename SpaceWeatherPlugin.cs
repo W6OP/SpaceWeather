8da9c847c0b3da9ba376909bf87fa1f72809379b
Peter Bourget <pbourget@w6op.com>
2021-10-06 11:43:37 -0700

    Updated build settings.

--- SpaceWeatherPlugin.cs
+++ SpaceWeatherPlugin.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Linq;
+﻿using System.ComponentModel.Composition;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using VE3NEA.HamCockpit.PluginAPI;
 
